399efcf82097de4cbf0efd14b05d9d9f3fa88094
Bryan Hoke <zinth@icloud.com>
2017-09-26 09:32:05 -0500

    Implement test methods for `ItemTemplateTests`.

--- Pokemon GO SimulatorTests/GAME_MASTER/ItemTemplateTests.swift
+++ Pokemon GO SimulatorTests/GAME_MASTER/ItemTemplateTests.swift
@@ -10,26 +10,53 @@ import XCTest
 
 class ItemTemplateTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testInitWithDictionaryLiteral() {
+        let key = "quick_moves"
+        let value1 = "VINE_WHIP_FAST"
+        let value2 = "TACKLE_FAST"
+        
+        var template: ItemTemplate = [
+            key: .string(value1),
+            key: .string(value2)
+        ]
+        
+        XCTAssertEqual(template[key], [value1, value2])
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSubscript() {
+        var template = ItemTemplate()
+        
+        do {
+            let key = "quick_moves"
+            let value1 = "VINE_WHIP_FAST"
+            let value2 = "TACKLE_FAST"
+            
+            template[key] = .string(value1)
+            template[key] = .string(value2)
+            
+            XCTAssertEqual(template[key], [value1, value2])
+        }
+        
+        do {
+            let key = "cinematic_moves"
+            let value1 = "SLUDGE_BOMB"
+            let value2 = "SEED_BOMB"
+            let value3 = "POWER_WHIP"
+            
+            template[key] = .string(value1)
+            template[key] = .string(value2)
+            template[key] = .string(value3)
+            
+            XCTAssertEqual(template[key], [value1, value2, value3])
+        }
+        
+        do {
+            let key = "pokemon_id"
+            let value: ItemTemplateValue = "BULBASAUR"
+            
+            template[key] = value
+            
+            XCTAssertEqual(template[key], value)
         }
     }
-    
 }
