f2d0dd85394a5bd4d505fd69ba5388844d0db7d2
Bryan Hoke <zinth@icloud.com>
2017-09-28 21:41:23 -0500

    Declare `CPCalculating`.

--- Pokemon GO Simulator/Utilities/Protocols.swift
+++ Pokemon GO Simulator/Utilities/Protocols.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+// MARK: - Calculating CP
+
 /// A type for which CP (combat power) can be calculated.
 protocol CPCalculable {
     
@@ -16,4 +18,11 @@ protocol CPCalculable {
     
     /// The `Stats` used for calculating CP.
     var stats: Stats { get }
+}
+
+/// A type which can calculate the CP (combat power) of `CPCalculable` instances.
+protocol CPCalculating {
+    
+    /// Calculates the CP of a `CPCalculable` instance.
+    func calculateCP(of subject: CPCalculable) -> Int
 }
